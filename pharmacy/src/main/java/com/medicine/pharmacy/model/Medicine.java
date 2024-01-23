75eb6ba968d38fd2881d7d71d2d90d54d46f0099
SladjanaColakovic <colakovic.ra32.2018@uns.ac.rs>
2024-01-23 16:38:23 +0100

    initial model created

--- pharmacy/src/main/java/com/medicine/pharmacy/model/Medicine.java
+++ pharmacy/src/main/java/com/medicine/pharmacy/model/Medicine.java
@@ -1,9 +1,6 @@
 package com.medicine.pharmacy.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,5 +14,16 @@ public class Medicine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String proprietaryName;
+    private String notProprietaryName;
+    private String indications;
+    private String sideEffects;
+    private String interactions;
+    private String applicationMethod;
+    private String composition;
+    @OneToOne
+    @JoinColumn(name = "classification_id", referencedColumnName = "id")
+    private MedicineClassification classification;
+
+
 }
